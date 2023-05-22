8a08303a8e1ecc6fb5f7558c78704b55a814acd7
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-23 00:39:08 +0500

    1st commit: beginning

--- my_tasks/Program.cs
+++ my_tasks/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int result = 1;
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Произведение чисел ");
+for(int i = 1; i <= num; i++)
+{
+    result = i * result;
+    if (i == num) Console.Write($"{i}");
+    else Console.Write($"{i}*");
+}
+Console.WriteLine($" равно {result}");
